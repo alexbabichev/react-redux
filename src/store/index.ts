be89573b25c8d724b89191880101dc9c0109cb6b
Alex Babichev <alex.babichev@me.com>
2019-03-07 18:28:32 +0100

    remote-redux-devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 import thunkMiddleware from "redux-thunk";
 
 import { systemReducer } from './system/system.redusers';
 
 const loggerMiddleware = createLogger();
-
 const rootReducer = combineReducers({
   system: systemReducer
 });
@@ -17,7 +17,7 @@ export default function configureStore() {
   const middlewares = [thunkMiddleware, loggerMiddleware];
   const store = createStore(
     rootReducer,
-    applyMiddleware(...middlewares)
+    composeWithDevTools(applyMiddleware(...middlewares))
   );
 
   return store;
