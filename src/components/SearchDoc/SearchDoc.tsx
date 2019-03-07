7cfa91e875de76104080206ccec4bb2b707e71aa
Alex Babichev <alex.babichev@me.com>
2019-03-07 18:45:47 +0100

    Issued Documents form

--- src/components/SearchDoc/SearchDoc.tsx
+++ src/components/SearchDoc/SearchDoc.tsx
@@ -12,8 +12,24 @@ class SearchDoc extends Component<Props> {
         <header className="BaseHeader">
           <h1>Issued Documents</h1>
         </header>
-
-
+        <section className="BaseContent">
+          <form className="BaseForm">
+            <p className="Label">Search for existing documents</p>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Document ID" value="" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Document Holder" value="" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Document Name" value="" />
+            </div>
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Issue Date" value="" />
+            </div>
+            <button className="btn btn-lg btn-primary">Submit</button>
+          </form>
+        </section>
       </section>
     )
   }
